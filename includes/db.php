7a6e230ffc939c6ee0306da29735923a85125281
akash8992 <akash8992@gmail.com>
2025-04-20 16:17:22 +0000

    run app

--- includes/db.php
+++ includes/db.php
@@ -1,16 +1,14 @@
 <?php
-// Database connection settings
-$servername = "localhost"; // The hostname of the database server
-$username = "root";        // The username to connect to the database
-$password = "";            // The password to connect to the database
-$dbname = "todo_list";     // The name of the database to connect to
+$host = 'mysql'; // this must match the service name in docker-compose
+$user = 'todo_user';
+$password = 'todo_pass';
+$database = 'todo_list';
 
-// Create a new MySQLi object to establish a database connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($host, $user, $password, $database);
 
-// Check if the connection was successful
+// Check connection
 if ($conn->connect_error) {
-    // If there is a connection error, output the error message and terminate the script
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
+
